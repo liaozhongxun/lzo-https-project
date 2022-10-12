2cd57e5f3ee8d015a8dcd85957a67bdb84d69cb9
liaozhongxun <869664233@qq.com>
2022-10-12 11:10:26 +0800

    需要https支持的功能

---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="home">
+        <button @click="gitLngLat">获取</button>
+        <div>{{ data }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HomeView",
+    data() {
+        return {
+            data: null,
+        };
+    },
+    methods: {
+        gitLngLat() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    (res) => {
+                        this.data = {
+                            lat: res.coords.latitude,
+                            lng: res.coords.longitude,
+                        };
+                    },
+                    (err) => {
+                        this.data = 'ERROR(' + err.code + '): ' + err.message;
+                    }
+                );
+            } else {
+                this.data = "该浏览器不支持定位";
+            }
+        },
+    },
+    mounted() {},
+};
+</script>
